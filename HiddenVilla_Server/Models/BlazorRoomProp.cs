54279e40e429465f34eb53a767547c0bcbe8dc0b
Diogo André Romão <diogoaromao@gmail.com>
2022-01-11 22:33:55 +0000

    Blazor Basics and Blazor Intermediate

---
+++ HiddenVilla_Server/Models/BlazorRoomProp.cs
@@ -0,0 +1,9 @@
+﻿namespace HiddenVilla_Server.Models
+{
+	public class BlazorRoomProp
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Value { get; set; }
+	}
+}

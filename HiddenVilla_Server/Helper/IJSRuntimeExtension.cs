c5c78ae6944365134d57992319463e4309e2a9ff
Diogo André Romão <diogoaromao@gmail.com>
2022-02-07 13:16:00 +0000

    Blazor and Javascript

---
+++ HiddenVilla_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Helper
+{
+	public static  class IJSRuntimeExtension
+	{
+		public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+		{
+			await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+		}
+
+		public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+		{
+			await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+		}
+
+		public static async ValueTask SwalSuccess(this IJSRuntime jsRuntime, string title, string message)
+		{
+			await jsRuntime.InvokeVoidAsync("ShowSwal", "success", title, message);
+		}
+
+		public static async ValueTask SwalError(this IJSRuntime jsRuntime, string title, string message)
+		{
+			await jsRuntime.InvokeVoidAsync("ShowSwal", "error", title, message);
+		}
+	}
+}
---
+++ HiddenVilla_Server/wwwroot/js/common.js
@@ -0,0 +1,23 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Operation Successful');
+    } else if (type === "error") {
+        toastr.error(message, 'Operation Failed');
+    }
+}
+
+window.ShowSwal = (type, title, message) => {
+    if (type === "success") {
+        Swal.fire(
+            title,
+            message,
+            'success'
+        );
+    } else if (type === "error") {
+        Swal.fire(
+            title,
+            message,
+            'error'
+        );
+    }
+}
